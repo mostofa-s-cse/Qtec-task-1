10d46ab43bb6976300578eb1f913e20fd0c01990
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-02-04 00:12:19 +0600

    login and signup page create

--- resources/views/back-end/layouts/master.blade.php
+++ resources/views/back-end/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Task-1</title>
+    <title>@yield('title')</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 Route::get('/', function () {
-    return view('back-end.dashboard');
+    return view('signup');
 });
